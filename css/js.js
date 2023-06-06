77f64726389815f4b5e4c66a3a61992c66a3e1f3
Lorin de Lepper <lorindelepper@gmail.com>
2023-06-06 12:55:58 +0200

    zfykh

--- css/js.js
+++ css/js.js
@@ -15,12 +15,13 @@ const listEl = document.querySelector('.mdc-drawer .mdc-list');
 const mainContentEl = document.querySelector('.main');
 
 
-
+// temp solution drawer
 function noClick () {
     document.querySelector('.main').classList.add('pointer');
 }
 
 // Openen en dicht doen van navigation drawer
+// het moment dat er op het icoontje word gedrukt voor de drawer gaat hij open
 document.querySelector('.mdc-top-app-bar__navigation-icon').addEventListener('click', (event)=>{drawer.open = true; noClick()});
 listEl.addEventListener('click', (event) => {
     drawer.open = false;
@@ -32,9 +33,11 @@ document.body.addEventListener('MDCDrawer:closed', () => {
 
 
 // Filter systeem
-function selectAllFunction(i) {
+// Selecteerd alle fotos en verbergt ze
+// De fotos die hetzelfde id hebben als e worden weer zichtbaar
+function selectAllFunction(e) {
     document.querySelectorAll('.mdc-image-list__item').forEach(elem => {elem.classList.add('hidden')});
-    document.querySelectorAll('.' + i).forEach(elem => {elem.classList.remove('hidden')});
+    document.querySelectorAll('.' + e).forEach(elem => {elem.classList.remove('hidden')});
 }
 
 
@@ -53,6 +56,7 @@ function openSheet(sheetID, source, title){
     // zorg dat de sheet geladen wordt voor de browser, naar de margin is deze nog niet in het zicht
     sheet.classList.remove('hidden');
     // even wachten zodat het element zeker zichtbaar is voor de browser (wel uit het zicht van de gebruiker) en geanimeerd kan worden
+    // zodat hij niet aan het laden is terwijl hij bezig is met animeren
     setTimeout(function(){ sheet.classList.remove('sheet-out-of-view'); }, 10);
 
     // als de homepage heel lang zou zijn kan je onder de sheet door scrollen, dat willen we als de sheet open is voorkomen (wel pas na animatie)
@@ -64,11 +68,16 @@ function openSheet(sheetID, source, title){
     let newURL = window.location.href + '?sheet=' + sheetID;
     history.pushState(null,null,newURL);
 
+    // verbergt de main zodat je niet meer naar beneden kan scrollen in de sheet
     document.querySelector('#main').classList.add('hidden');
 
+    // zet de titel  en foto op de juiste plek
     document.querySelector('#meme-image').src = source;
     document.querySelector('#meme-title').innerText = title;
 }
+
+
+
 function closeSheet(){
     // verwijder eerste "alle" sheets weer uit het zicht met een animatie en daarna ook niet meer weergeven in browser
     let sheets = document.querySelectorAll('.sheet');
@@ -77,6 +86,7 @@ function closeSheet(){
         setTimeout(function() { sheet.classList.add('hidden'); }, 310);
     });
 
+    // maakt main weer zichtbaar zodat je weer de andere memes kan zien
     document.querySelector('#main').classList.remove('hidden');
 
     // url weer aanpassen naar home voor de zekerheid
